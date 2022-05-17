d0bad799c1d597bff50c39bcc0593911d04f4521
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-05-17 14:09:48 +0300

    added most blogs test / exercise 4.6

--- part4/server/utils/list_helper.js
+++ part4/server/utils/list_helper.js
@@ -1,17 +1,28 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
 
 const totalLikes = (blogs) => {
-    let sum =  blogs.reduce(((total, blog) => {return total + blog.likes}), 0)
+    let sum = blogs.reduce(((total, blog) => { return total + blog.likes }), 0)
     return sum
 }
 
 const favoriteBlog = (blogs) => {
-    if(blogs.length === 0) return null
-    
+    if (blogs.length === 0) return null
+
     let favorite = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
     return favorite
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return null
+    const arrayOfAuthors = blogs.map((blog) => { return blog.author })
+    return _.head(_(arrayOfAuthors)
+        .countBy()
+        .entries()
+        .maxBy(_.last))
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
