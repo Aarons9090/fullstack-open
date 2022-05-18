463e6ef1b3d219025a027b8e2f6b8a2583adf03b
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-05-18 11:23:45 +0300

    refactored app into app.js

---
+++ part4/server/app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const config = require("./utils/config")
+const blogsRouter = require("./controllers/blogs")
+const logger = require("./utils/logger")
+const middleware = require("./utils/middleware")
+
+console.log(config.MONGOURL)
+mongoose.connect(config.MONGOURL)
+    .then(() => {
+        logger.info("connected to MongoDB")
+    })
+    .catch((error) => {
+        logger.error("error while connecting to MongoDB:", error)
+    })
+
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknownRequest)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/server/index.js
+++ part4/server/index.js
@@ -1,25 +1,10 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require("http")
+const app = require("./app")
 const config = require("./utils/config")
-const blogsRouter = require('./controllers/blogs')
 const logger = require("./utils/logger")
-const middleware = require("./utils/middleware")
 
-console.log(config.MONGOURL)
-mongoose.connect(config.MONGOURL)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownRequest)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
