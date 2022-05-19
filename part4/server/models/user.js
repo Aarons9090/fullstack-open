968ee6d25c7df2abc5d2a0c430e3d37903f40ad6
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-05-19 11:16:53 +0300

    implemented users POST + GET method / exercise 4.15

---
+++ part4/server/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    passwordHash: String,
+    
+})
+
+userSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject.__v
+        delete returnedObject._id
+        delete returnedObject.passwordHash
+    }
+})
+  
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
