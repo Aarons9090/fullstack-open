5d10830faa9099b313f89b1cc819afb11582f9f4
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-05-19 12:11:00 +0300

    added blog + user data union + populate method / exercise 4.17

--- part4/server/controllers/blogs.js
+++ part4/server/controllers/blogs.js
@@ -1,25 +1,32 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
 require("express-async-errors")
 
 blogsRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate("user", {username: 1, name: 1, id: 1})
     response.json(blogs)
 })
 
 blogsRouter.post("/", async (request, response) => {
     const body = request.body
+
+    const user = (await User.find({}))[0] // find first user in users *temporary*
+
     const blog = new Blog({
         author: body.author,
         title: body.title,
         url: body.url,
         likes: body.likes ? body.likes : 0,
+        user: user._id
     })
     
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 
-    
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
--- part4/server/controllers/users.js
+++ part4/server/controllers/users.js
@@ -2,9 +2,11 @@ const usersRouter = require("express").Router()
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
 require("express-async-errors")
+require("mongoose")
 
 usersRouter.get("/", async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate("blogs", {url: 1, title: 1, author: 1, id: 1})
+    console.log(users)
     response.json(users)
 })
 
@@ -22,7 +24,7 @@ usersRouter.post("/", async (request, response) => {
     if(existingUser){
         return response.status(400).json({error: "username already exists"})
     }
-    
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
