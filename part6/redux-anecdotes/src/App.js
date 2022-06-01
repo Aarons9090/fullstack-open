8b676636d2e82866a99cb98390d035577cb54dcd
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-06-01 15:29:53 +0300

    added anecdote exercise + voteing and adding of new anecdotes with reducers / exercise 6.3+6.4

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote, addNew } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const voteAnecdote = (id) => {
+    dispatch(vote(id))
+  }
+
+  const addNewAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ""
+    dispatch(addNew(content))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => voteAnecdote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addNewAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
