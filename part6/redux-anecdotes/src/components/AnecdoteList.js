ef1818c4f0a9f7b0fb2fd10d4653f565bd0b155c
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-06-02 12:33:47 +0300

    added filter reducer + filtering of anecdotes / exercise 6.12

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,11 +1,12 @@
-import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { vote } from "../reducers/anecdoteReducer"
 import { addNotification, clearNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = (props) => {
     const anecdotes = useSelector(state => state.anecdotes)
+    const filter = useSelector(state=> state.filter)
     const dispatch = useDispatch()
+
     const voteAnecdote = ({id, content}) => {
         dispatch(vote(id))
         
@@ -14,12 +15,17 @@ const AnecdoteList = (props) => {
              dispatch(clearNotification())
          }, 5000)
         
-        
       }
+
+    const anecdotesToShow = () => {
+        return anecdotes
+            .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+         
+    }
 
     return (
         <div>
-            {anecdotes.sort((a, b) => { return b.votes - a.votes }).map(anecdote =>
+            {anecdotesToShow().map(anecdote =>
                 <div key={anecdote.id}>
                     <div>
                         {anecdote.content}
