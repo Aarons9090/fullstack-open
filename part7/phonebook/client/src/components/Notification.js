8a184e7536ac644cd0d36751f4c69b73a0291203
Aaron Hirvi <83041434+Aarons9090@users.noreply.github.com>
2022-06-10 23:06:38 +0300

    Delete part7/phonebook directory

--- part7/phonebook/client/src/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux"
-
-const Notification = () => {
-    const message = useSelector((state) => state.notification)
-
-    return message ? <div className={message.class}>{message.text}</div> : null
-}
-
-export default Notification
--- part7/phonebook/client/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-const URL = "/api/login"
-
-const login = async credentials => {
-    const response = await axios.post(URL, credentials)
-    return response.data
-}
-
-export default { login }
